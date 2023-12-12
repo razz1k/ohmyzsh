eccfdf9c7cf0d81d91a326d0cd23eb541ed2ebfb
razz1k <zdeadmenz@gmail.com>
2023-12-12 16:43:44 +0300

    add upstream if not exist

--- custom/syncMyOmzFork.zsh
+++ custom/syncMyOmzFork.zsh
@@ -1,5 +1,12 @@
 syncMyOmzFork() {
   curDir=$(pwd)
-  cd /home/$USER/.oh-my-zsh && git fetch upstream && git merge upstream/master && git push
+  cd /home/$USER/.oh-my-zsh
+
+  if ! git ls-remote --exit-code upstream >/dev/null 2>&1 ; then
+    git remote add upstream git@github.com:ohmyzsh/ohmyzsh.git
+  fi
+
+  git fetch upstream && git merge upstream/master && git push
   cd "$curDir" || return
 }
+
