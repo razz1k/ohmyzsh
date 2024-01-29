f06d06c96763c99469a52b0a772a348ab82fb016
razz1k <zdeadmenz@gmail.com>
2024-01-29 20:13:01 +0300

    coomit only after pass linters for SU

--- custom/setProfile.sh
+++ custom/setProfile.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
-if [[ ! ( ('balanced' == $1) || ('low-power' == $1) || ('performance' == $1) ) ]]; then
-  exit
-fi
+case $1 in
+  balanced)
+  ;;
+  low-power)
+  ;;
+  performance)
+  ;;
+  *)
+    exit
+  ;;
+esac
 
 bash -c "echo $1 > /sys/firmware/acpi/platform_profile"
