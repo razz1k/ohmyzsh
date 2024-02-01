6651c7f8da3a4e719a835ca59467f7925d86a3e5
razz1k <zdeadmenz@gmail.com>
2024-02-01 11:06:56 +0300

    bring back old logic of ggCommit

--- custom/gitAliases.zsh
+++ custom/gitAliases.zsh
@@ -1,3 +1,3 @@
 alias ggPull='git pull origin "$(git_current_branch)"'
 alias ggPush='git push origin "$(git_current_branch)"'
-alias ggCommit='[[ "$(pwd)" == *"SavingsUnited"* ]] && yarn && yarn lint; git commit --message'
+alias ggCommit='[[ "$(pwd)" == *"SavingsUnited"* ]] && yarn; git commit --message'
